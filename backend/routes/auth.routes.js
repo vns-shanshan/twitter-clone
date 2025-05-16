968dd7f3a55591572178c5c0c048646df2d74dab
huijingj <vanessajin0426@gmail.com>
2025-05-16 15:58:40 -0700

    1 - auth controller added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, getMe } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.post("/signup", signup)
 router.post("/login", login)
 
 router.post("/logout", logout)
+
+router.get("/me", protectRoute, getMe)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -9,6 +10,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parser req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
