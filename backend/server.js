ee93eb525139fa0c605f80a47010d5eda42e6af2
huijingj <vanessajin0426@gmail.com>
2025-05-21 15:42:36 -0700

    1 - update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
