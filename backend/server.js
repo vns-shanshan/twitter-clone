bc00bc1c0f4016f086436a347ab09dde65c10d92
huijingj <vanessajin0426@gmail.com>
2025-05-21 16:00:50 -0700

    1 - notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
