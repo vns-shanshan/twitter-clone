378e7331d43d49477e492b87065dbbc267f1e5f6
huijingj <vanessajin0426@gmail.com>
2025-05-27 15:30:52 -0700

    3 - useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -41,7 +41,6 @@ export const followUnfollowUser = async (req, res) => {
             await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
             await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-            // TODO: return the id of the user as a response
             res.status(200).json({ message: "User unfollowed successfully" });
 
         } else {
@@ -60,7 +59,6 @@ export const followUnfollowUser = async (req, res) => {
             });
             await newNotification.save();
 
-            // TODO: return the id of the user as a response
             res.status(200).json({ message: "User followed successfully" });
         }
     } catch (error) {
