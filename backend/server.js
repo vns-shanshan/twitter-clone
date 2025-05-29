0f3e251e092d32ca1a77d0720debe9af91e242f0
huijingj <vanessajin0426@gmail.com>
2025-05-29 09:24:34 -0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get(/^\/(?!api).*/, (req, res) => {
+    app.get("/{*any}", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
